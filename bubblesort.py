e66447e00a573988957f4953eba103361ab09f33
bjchu5 <bjchu5@users.noreply.github.com>
2016-09-06 14:34:06 +0800

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,5 +1,5 @@
 def bubbleSort(aList):
-        for i in range(0, len(aList)):
+        for i in range(0, len(aList)-1):
                 for j in range(0, len(aList) - i -1):
                         if aList[j] > aList[j+1]:
                                 aList[j], aList[j+1] = aList[j+1], aList[j]
